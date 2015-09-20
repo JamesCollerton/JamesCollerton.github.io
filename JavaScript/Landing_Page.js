466945e4fc9691cb51a5ada0d287c5da4b0621aa
James Collerton <jamescollerton@live.co.uk>
2015-09-20 14:07:36 +0100

    Playing with opacities

--- JavaScript/Landing_Page.js
+++ JavaScript/Landing_Page.js
@@ -1,2 +1,20 @@
-document.getElementById("education_img").style.color = "blue";
-document.getElementById("education_text").style.color = "blue";
+
+function hover_on_education(){
+
+	document.getElementById("education_img").style.color = "blue";
+	document.getElementById("education_text").style.color = "blue";
+
+}
+
+function isHover(e) {
+
+	is_hovering = ( e.parentElement.querySelector(':hover') === e )
+
+    return (is_hovering);
+
+}
+setInterval(function () {
+
+    console.log(isHover(document.getElementById('img_hover_wrap')));
+
+}, 100);
