b410acc58617acba56a6c82a68ad8ba2d271f7dc
James Collerton <jamescollerton@live.co.uk>
2015-09-20 14:14:49 +0100

    Trying out JS, removing HTML/CSS

--- JavaScript/Landing_Page.js
+++ JavaScript/Landing_Page.js
@@ -1,8 +1,15 @@
 
 function hover_on_education(){
 
-	document.getElementById("education_img").style.opacity = 0;
+	document.getElementById("education_img").style.opacity = 0.15;
 	document.getElementById("education_txt").style.opacity = 1;
+
+}
+
+function not_hover_on_education(){
+
+	document.getElementById("education_img").style.opacity = 1;
+	document.getElementById("education_txt").style.opacity = 0;
 
 }
 
@@ -18,5 +25,6 @@ setInterval(function () {
     education_hovering = isHover(document.getElementById('img_hover_wrap'));
 
     if(education_hovering){ hover_on_education(); }
+    else{ not_hover_on_education(); }
 
 }, 100);
