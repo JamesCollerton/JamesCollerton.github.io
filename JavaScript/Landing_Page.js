294c5dc94e7bdf5c59096894b0481ca0442af48b
James Collerton <jamescollerton@live.co.uk>
2015-09-20 14:24:33 +0100

    Made a lot of changes to JS

--- JavaScript/Landing_Page.js
+++ JavaScript/Landing_Page.js
@@ -1,8 +1,8 @@
 
-function hover_on_education(img_opacity, txt_opacity){
+function hover_on(img_id, txt_id, img_opacity, txt_opacity){
 
-	document.getElementById("education_img").style.opacity = img_opacity;
-	document.getElementById("education_txt").style.opacity = txt_opacity;
+	document.getElementById(img_id).style.opacity = img_opacity;
+	document.getElementById(txt_id).style.opacity = txt_opacity;
 
 }
 
@@ -15,9 +15,17 @@ function isHover(e) {
 }
 setInterval(function () {
 
-    education_hovering = isHover(document.getElementById('img_hover_wrap'));
+    education_hovering = isHover(document.getElementById('img_hover_wrap_education'));
+    project_hovering = isHover(document.getElementById('img_hover_wrap_projects'));
+    about_hovering = isHover(document.getElementById('img_hover_wrap_about'));
 
-    if(education_hovering){ hover_on_education(0.15, 1); }
-    else{ hover_on_education(1, 0); }
+    if(education_hovering){ hover_on("education_img", "education_txt", 0.15, 1); }
+    else{ hover_on("education_img", "education_txt", 1, 0); }
+
+    if(project_hovering){ hover_on("project_img", "project_txt", 0.15, 1); }
+    else{ hover_on("project_img", "project_txt", 1, 0); }
+
+    if(education_hovering){ hover_on_education("about_img", "about_txt", 0.15, 1); }
+    else{ hover_on("about_img", "about_txt", 1, 0); }
 
 }, 100);
