aadde556a2097846eab8297a1882759bb7cafec0
James Collerton <jamescollerton@live.co.uk>
2016-07-28 18:57:55 +0100

    Added SASS and sorting out Education page

---
+++ SASS/sassCompilation.sh
@@ -0,0 +1,5 @@
+# Bash script for compiling all of the SASS
+
+sass education.scss ../CSS/CSS_2/education.css
+sass common.scss ../CSS/CSS_2/common.css
+sass landingPage.scss ../CSS/CSS_2/landingPage.css
