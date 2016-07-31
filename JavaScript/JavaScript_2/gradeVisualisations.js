83030ada1152eaa3d2d6a4c52fc04a88b7dd3f25
James Collerton <jamescollerton@live.co.uk>
2016-07-31 11:01:57 +0100

    Cleaned up grade vis code

--- JavaScript/JavaScript_2/gradeVisualisations.js
+++ JavaScript/JavaScript_2/gradeVisualisations.js
@@ -1,10 +1,29 @@
-// Code to create the visualisations.
+/**
 
+@class gradeVisualisations
+
+@classdesc Holds all of the information around creating the grade visualisations.
+
+*/
 function gradeVisualisations() {
 
+	/**
+
+	@memberof gradeVisualisations
+	@inner previousButtonID
+	@description Holds the ID of the last clicked button. An empty string if
+	nothing has been clicked.
+
+	*/
 	var previousButtonID = "";
 
-	// Creates a vector of all the tabs we want to look at.
+	/**
+
+	@memberof gradeVisualisations
+	@inner sectionButtons
+	@description Dictionary of all the button IDs we can click on the page.
+
+	*/
 	var sectionButtons = {
 
 		mscSection : "educationPageMScIcon",
@@ -13,9 +32,18 @@ function gradeVisualisations() {
 
 	};
 
-	// Adds a click listener to each of the buttons. Adds a short delay on the
-	// animation so that the right tab is counted as active.
-	function assign_button_listener(tsv_data){
+	/**
+
+	@function assignButtonListeners
+
+	@param tsv_data - The data we want to draw.
+
+	@description This function assigns all of the button listeners
+	to the different stats buttons, changing the graph and updating
+	the CSS of the clicked ones.
+
+	*/
+	function assignButtonListeners(tsv_data){
 
 		for(button in sectionButtons){
 
@@ -33,7 +61,17 @@ function gradeVisualisations() {
 
 	}
 
-// 
+	/**
+
+	@function assignButtonListeners
+
+	@param buttonID - The ID of the button that was clicked.
+
+	@description If a button was previously marked as active, removes that
+	CSS. Then adds the CSS to the last clicked button and sets the 
+	previously clicked button.
+
+	*/
 	function changeButtonCSS(buttonID){
 
 		if(previousButtonID !== ""){ 
@@ -46,7 +84,18 @@ function gradeVisualisations() {
 
 	}
 
-	// C
+	/**
+
+	@function assignButtonListeners
+
+	@param data - The data for the grades.
+	@param buttonID - The ID of the button that was clicked.
+
+	@description We assign the sliced data as the data itself, then depending
+	on what was clicked we change it to reflect that selection and then draw
+	it in the graph.
+
+	*/
 	function changeGradeVisualisation(data, buttonID){
 
 		var slice = data;
@@ -69,31 +118,53 @@ function gradeVisualisations() {
 
 	}
 
+	// Functions to be accessible outside the class.
 	return {
-		assign_button_listener: assign_button_listener
+		assignButtonListeners: assignButtonListeners
 	}
 
 }
 
+/**
+
+@class d3Graph
+
+@classdesc Holds all of the information around the actual d3 graph.
+
+*/
 function d3Graph(){
 
-	var d3GraphData = [];
+	/**
 
-	// Mike Bostock "margin conventions"
+	@memberof d3Graph
+	@inner margin
+	@description These are the margins assigned to the graph. 
+
+	*/
 	var margin = {top: 20, right: 20, bottom: 70, left: 40},
 				  width = 500 - margin.left - margin.right,
 				  height = 400 - margin.top - margin.bottom;
 
-	// D3 scales = just math
-	// x is a function that transforms from "domain" (data) into "range" (usual pixels)
-	// domain gets set after the data loads
+	/**
+
+	@memberof d3Graph
+	@inner x / y
+	@description These are the scales for the graph. 
+
+	*/
 	var x = d3.scale.ordinal()
 				.rangeRoundBands([0, width], .1);
 
 	var y = d3.scale.linear()
 				.range([height, 0]);
 
-	// D3 Axis - renders a d3 scale in SVG
+	/**
+
+	@memberof d3Graph
+	@inner xAxis / yAxis
+	@description These are the axes for the graph.
+
+	*/
 	var xAxis = d3.svg.axis()
 					.scale(x)
 					.orient("bottom");
@@ -103,11 +174,14 @@ function d3Graph(){
 					.orient("left")
 					.ticks(10, "%");
 
-	// create an SVG element (appended to body)
-	// set size
-	// add a "g" element (think "group")
-	// annoying d3 gotcha - the 'svg' variable here is a 'g' element
-	// the final line sets the transform on <g>, not on <svg>
+	/**
+
+	@memberof d3Graph
+	@inner svg
+	@description This is the SVG element, sets the width, height and adds a 
+	group. Then adds the x and y axes with some labelling.
+
+	*/
 	var svg = d3.select("#d3_graph").append("svg")
 					.attr("width", width + margin.left + margin.right)
 					.attr("height", height + margin.top + margin.bottom)
@@ -120,41 +194,65 @@ function d3Graph(){
 
 	svg.append("g")
 			.attr("class", "y axis")
-			.append("text") // just for the title (ticks are automatic)
-			.attr("transform", "rotate(-90)") // rotate the text!
+			.append("text") 
+			.attr("transform", "rotate(-90)")
 			.attr("y", 6)
 			.attr("dy", ".71em")
 			.style("text-anchor", "end")
 			.text("Percentage");
 
-	// d3.tsv is a wrapper around XMLHTTPRequest, returns array of arrays (?) for a TSV file
-	// type function transforms strings to numbers, dates, etc.
+	/**
+
+	@memberof d3Graph
+	@inner d3.tsv
+	@description d3.tsv is a wrapper around XMLHTTPRequest, returns array of 
+	arrays (?) for a TSV file type function transforms strings to numbers, 
+	dates, etc.
+
+	*/
 	d3.tsv("Data/data.tsv", type, function(error, data) {
-		d3GraphData = data;
 		msc_slice = data.slice(0, 7)
 		draw(msc_slice)
-		educationGradeVisualisations.assign_button_listener(data)
+		educationGradeVisualisations.assignButtonListeners(data)
 	});
 
+	/**
+
+	@memberof d3Graph
+	@inner svg
+	@description This is the SVG element, sets the width, height and adds a 
+	group. Then adds the x and y axes with some labelling.
+
+	*/
 	function type(d) {
-		// + coerces to a Number from a String (or anything)
 		d.frequency = +d.frequency;
 		return d;
 	}
 
+	/**
+
+	@function assignButtonListeners
+
+	@param data - The data for the grades.
+	@param buttonID - The ID of the button that was clicked.
+
+	@description This redraws the graph.
+
+	*/
 	function draw(data) {
-		// measure the domain (for x, unique letters) (for y [0,maxFrequency])
-		// now the scales are finished and usable
+
+		// Measure the domain (for x, unique letters) (for y [0,maxFrequency])
+		// now the scales are finished and usable.
 		x.domain(data.map(function(d) { return d.letter; }));
 		y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
 
-		// another g element, this time to move the origin to the bottom of the svg element
-		// someSelection.call(thing) is roughly equivalent to thing(someSelection[i])
-		//   for everything in the selection\
-		// the end result is g populated with text and lines!
+		// Another g element, this time to move the origin to the bottom of 
+		// the svg element someSelection.call(thing) is roughly equivalent to 
+		// thing(someSelection[i]) for everything in the selection the end 
+		// result is g populated with text and lines!
 		svg.select('.x.axis').transition().duration(300).call(xAxis);
 
-		// same for yAxis but with more transform and a title
+		// Same for yAxis but with more transform and a title
 		svg.select(".y.axis").transition().duration(300).call(yAxis)
 
 		svg.select('.x.axis')
@@ -166,11 +264,10 @@ function d3Graph(){
 					return "rotate(-65)" 
 			});
 
-		// THIS IS THE ACTUAL WORK!
 		var bars = svg.selectAll(".bar")
 						.data(data, function(d) { 
 							return d.letter; 
-						}) // (data) is an array/iterable thing, second argument is an ID generator function
+						})
 
 		bars.exit()
 			.transition()
@@ -180,29 +277,26 @@ function d3Graph(){
 			.style('fill-opacity', 1e-6)
 			.remove();
 
-		// data that needs DOM = enter() (a set/selection, not an event!)
+		// Data that needs DOM = enter() (a set/selection, not an event!)
 		bars.enter().append("rect")
 			.attr("class", "bar")
 			.attr("y", y(0))
 			.attr("height", height - y(0));
 
-		// the "UPDATE" set:
-		bars.transition().duration(300).attr("x", function(d) { return x(d.letter); }) // (d) is one item from the data array, x is the scale object from above
-			.attr("width", x.rangeBand()) // constant, so no callback function(d) here
+		// The "UPDATE" set:
+		bars.transition().duration(300).attr("x", function(d) { return x(d.letter); }) 
+			.attr("width", x.rangeBand()) 
 			.attr("y", function(d) { return y(d.frequency); })
-			.attr("height", function(d) { return height - y(d.frequency); }); // flip the height, because y's domain is bottom up, but SVG renders top down
+			.attr("height", function(d) { return height - y(d.frequency); }); 
 
 	}
 
-	function getData(){
-		return d3GraphData;
-	}
-
+	// Functions available outside class.
 	return{
-		draw: draw,
-		getData: getData
+		draw: draw
 	}
 }
 
+// Initialising closures.
 var educationGradeVisualisations = gradeVisualisations();
 var educationD3Graph = d3Graph();
