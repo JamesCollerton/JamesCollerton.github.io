f25357df327c2221f2036253cb74f70c12c030c6
James Collerton <jamescollerton@live.co.uk>
2016-08-06 09:23:38 +0100

    Adding navbar width

--- JavaScript/JavaScript_2/gradeVisualisations.js
+++ JavaScript/JavaScript_2/gradeVisualisations.js
@@ -232,10 +232,7 @@ function d3Graph(d3GraphID){
 		if(d3GraphID === "d3GraphTwo"){ return 250; }
 
 		var distanceFromTop = $("#" + d3GraphID).offset().top;
-		var heightOfDocument = $(window).height();
-
-		console.log(distanceFromTop);
-		console.log(heightOfDocument);
+		var heightOfDocument = $(window).height() - 100;
 
 		var heightOfElement = heightOfDocument - (distanceFromTop * 2); 
 
