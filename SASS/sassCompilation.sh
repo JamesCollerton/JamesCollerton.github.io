7f881c78720256b05a24ec8d0276cd63381d6502
James Collerton <jamescollerton@live.co.uk>
2016-09-11 12:15:30 +0100

    Made some progress on the about page

--- SASS/sassCompilation.sh
+++ SASS/sassCompilation.sh
@@ -4,3 +4,4 @@ sass education.scss ../CSS/CSS_2/education.css
 sass common.scss ../CSS/CSS_2/common.css
 sass landingPage.scss ../CSS/CSS_2/landingPage.css
 sass projects.scss ../CSS/CSS_2/projects.css
+sass about.scss ../CSS/CSS_2/about.css
