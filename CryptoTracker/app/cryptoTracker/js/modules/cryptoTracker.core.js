0ca7b10ce24317cdc6b04a05a0c504b02c82a8d5
James Collerton <James.Collerton@kobaltmusic.com>
2018-01-15 13:00:39 +0000

    Started the crypto tracker

---
+++ CryptoTracker/app/cryptoTracker/js/modules/cryptoTracker.core.js
@@ -0,0 +1,13 @@
+/*
+	@name cryptoTrackerCore
+
+	@description This holds all of the initialisation of the modules used in
+	the webpage.
+ */
+
+ /*
+	@ngdoc directive
+	@description This is an overarching module for all of the contained modules in
+	the app.
+ */
+angular.module('cryptoTrackerApp', [])
