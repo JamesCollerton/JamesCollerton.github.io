9ba62772c27292f56e70380304ef8bb06d3a845c
James Collerton <jamescollerton@live.co.uk>
2018-01-15 22:01:56 +0000

    Midway push

--- CryptoTracker/app/cryptoTracker/js/modules/cryptoTracker.core.js
+++ CryptoTracker/app/cryptoTracker/js/modules/cryptoTracker.core.js
@@ -10,13 +10,24 @@
 	@description This is an overarching module for all of the contained modules in
 	the app.
  */
-angular.module('cryptoTrackerApp', ['cryptoTrackerHeaderApp'])
+angular.module('cryptoTrackerApp', ['cryptoTrackerHeaderApp', 'cryptoTrackerTableApp'])
 
 /*
 	@ngdoc directive
 	@description This is used to initialise the header for the page.
  */
 angular.module('cryptoTrackerHeaderApp', []).controller('HeaderCtrl', function MainCtrl() {
+  this.header = {
+	name: 'Crypto Tracker',
+	description: 'Crypto Decrypted'
+  };
+});
+
+/*
+	@ngdoc directive
+	@description This is used to initialise the table for the app.
+ */
+angular.module('cryptoTrackerTableApp', []).controller('TableCtrl', function MainCtrl() {
   this.header = {
 	name: 'Crypto Tracker',
 	description: 'Crypto Decrypted'
--- CryptoTracker/app/cryptoTracker/js/modules/cryptoTracker.table.js
+++ CryptoTracker/app/cryptoTracker/js/modules/cryptoTracker.table.js
@@ -1,5 +1,5 @@
 /*
-	@name cryptoTrackerHeader
+	@name cryptoTrackerTable
 	@description This holds all of the angular directives around the header
 	for the page.
  */
@@ -8,9 +8,9 @@
 	@ngdoc directive
 	@description This is used to initialise the header for the page.
  */
-angular.module('cryptoTrackerHeaderApp').component('headerDetail', {
-  templateUrl: 'CryptoTracker/app/cryptoTracker/html/cryptoTracker.header.html',
-  controller: HeaderDetailController,
+angular.module('cryptoTrackerTableApp').component('headerDetail', {
+  templateUrl: 'CryptoTracker/app/cryptoTracker/html/cryptoTracker.table.html',
+  controller: TableDetailController,
   bindings: {
     header: '='
   }
