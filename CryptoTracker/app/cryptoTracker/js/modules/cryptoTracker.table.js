a8e89ac43e2506a27e8c8ef5bfa718bda92278bf
James Collerton <jamescollerton@live.co.uk>
2018-01-15 22:27:14 +0000

    Midway push

--- CryptoTracker/app/cryptoTracker/js/modules/cryptoTracker.table.js
+++ CryptoTracker/app/cryptoTracker/js/modules/cryptoTracker.table.js
@@ -8,7 +8,7 @@
 	@ngdoc directive
 	@description This is used to initialise the header for the page.
  */
-angular.module('cryptoTrackerTableApp').component('headerDetail', {
+angular.module('cryptoTrackerTableApp').component('tableDetail', {
   templateUrl: 'CryptoTracker/app/cryptoTracker/html/cryptoTracker.table.html',
   controller: TableDetailController,
   bindings: {
