f2593b283a90f88e7ce24251aa9f07d247f4b7ab
James Collerton <jamescollerton@live.co.uk>
2018-01-18 20:49:12 +0000

    Changed detail to table

--- CryptoTracker/app/cryptoTracker/js/modules/cryptoTracker.panel.js
+++ CryptoTracker/app/cryptoTracker/js/modules/cryptoTracker.panel.js
@@ -8,7 +8,7 @@
 	@ngdoc directive
 	@description This is used to initialise the header for the page.
  */
-angular.module('cryptoTrackerPanelApp').component('panelDetail', {
+angular.module('cryptoTrackerTableApp').component('tableDetail', {
   templateUrl: 'CryptoTracker/app/cryptoTracker/html/cryptoTracker.panel.html',
   controller: TableDetailController,
   bindings: {
