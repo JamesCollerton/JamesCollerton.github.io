c86848d0998a0da39e5fddf79efc5850ffe1da97
James Collerton <James.Collerton@kobaltmusic.com>
2018-01-22 13:37:30 +0000

    Added scope and http to table controller

--- CryptoTracker/app/cryptoTracker/js/controllers/cryptoTracker.table.js
+++ CryptoTracker/app/cryptoTracker/js/controllers/cryptoTracker.table.js
@@ -1,8 +1,8 @@
 /*
  * @name intelligentShuffleHeader
  * @description This holds the controller for the page header.
- * 
+ *
  */
-function TableDetailController() {
+function TableDetailController($scope, $http) {
 
 }
